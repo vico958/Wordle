f025dd0eed8576fd258b22879c48c0544b5c8a48
vico958 <vico958@gmail.com>
2023-12-04 00:31:26 +0200

    checking update

--- guesstheword/server/Server.js
+++ guesstheword/server/Server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const game = require("./Server/Routes/Game/Game");
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/game", game);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
